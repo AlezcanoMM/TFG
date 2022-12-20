ae163ac44c98761d52a9f52345c6516f5824a23e
Alezcanomm <Alezcanomm@gmail.com>
2022-12-20 11:42:05 +0000

    Version final

--- Assets/Scripts/Controllers/GeneralControllers/RoomSynchController.cs
+++ Assets/Scripts/Controllers/GeneralControllers/RoomSynchController.cs
@@ -57,6 +57,7 @@ public class RoomSynchController : MonoBehaviourPunCallbacks
     [PunRPC]
     private void CreateUserButtonRPC(Player localPlayer, string username) {
         GameObject userButton = PhotonNetwork.Instantiate("UserButton", Vector3.zero, Quaternion.Euler(-90, 0, 0), 0);
+        userButton.SetActive(false);
 
         //Delegate to button that if roomMaster clicks it, you can forfeit roomMaster
         userButton.GetComponent<InterfaceController>().label.text = username;
